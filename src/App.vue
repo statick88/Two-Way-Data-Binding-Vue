158f0526c9ca2859324307f59babcafdf676b767
Diego Saavedra <dmsaavedra@espe.edu.ec>
2024-01-16 16:43:48 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <TwoWayBinding />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TwoWayBinding from '@/components/TwoWayBinding.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TwoWayBinding
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Estilos globales de la aplicación */
 </style>
---
+++ src/components/TwoWayBinding.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <input type="text" v-model="message">
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello'
+        };
+    }
+};
+</script>
+
+<style scoped>
+/* Estilos específicos del componente */
+</style>
